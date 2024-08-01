134006056cabecbfbb08cf3c5d9b2800b4db1f86
Mahsa <jamalimahsa1377@gmail.com>
2024-08-01 12:22:24 +0200

    third

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -10,7 +10,7 @@ const ProjectsCard = ({ url, img, githuburl, title, text }) => {
             />
             <div className="capitalize p-8">
                 <h2 className='text-xl tracking-wide font-medium'>{title}</h2>
-                {/* <p className='mt-4 text-slate-700 leading-loose'>{text}</p> */}
+                 <p className='mt-4 text-slate-700 leading-loose'>{text}</p> 
                 <div className='mt-4 flex gap-x-4'>
                     <a href={url}>
                         <TbWorldWww className='h-8 w-8 text-slate-500
